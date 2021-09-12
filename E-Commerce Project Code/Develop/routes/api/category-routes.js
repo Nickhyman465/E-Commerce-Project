bb1b5f6ddd1b32405e13e52c7fc8b55e8acc7434
Nickhyman465 <nickhyman465@gmail.com>
2021-09-11 23:58:18 -0400

    tag-route completed

--- E-Commerce Project Code/Develop/routes/api/category-routes.js
+++ E-Commerce Project Code/Develop/routes/api/category-routes.js
@@ -3,14 +3,34 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // finds all categories
+  // includes its associated Products
+try {
+  const categoryData = await Category.findAll({
+    include: [{ model: Product, as: "merchandise" }]
+  });
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // finds one category by its `id` value
+  //includes its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product, as: "merchandise"}]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
--- E-Commerce Project Code/Develop/routes/api/tag-routes.js
+++ E-Commerce Project Code/Develop/routes/api/tag-routes.js
@@ -4,27 +4,28 @@ const { Tag, Product, ProductTag } = require("../../models");
 // The `/api/tags` endpoint
 
 router.get("/", async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  // finds all tags
+  // includes its associated Product data
   try {
     const tagData = await Tag.findAll({
       // JOIN with Product, using the ProductTag through table
-      include: [{ model: Product, through: ProductTag, as: "taggedProducts" }]
+      include: [{ model: Product, through: ProductTag, as: "productTags" }]
     });
 
     res.status(200).json(tagData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
 
 router.get("/:id", async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // finds a single tag by its `id`
+  // includes its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       // JOIN with Product, using the ProductTag through table
-      include: [{ model: Product, through: ProductTag, as: "taggedProducts" }]
+      include: [{ model: Product, through: ProductTag, as: "productTags" }]
     });
 
     if (!tagData) {
@@ -39,7 +40,7 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  // create a new tag
+  // creates a new tag
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
@@ -49,7 +50,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  // update a tag's name by its `id` value
+  // updates a tag's name by its `id` value
   try {
     const updatedTagData = await Tag.update(
       {
@@ -64,7 +65,7 @@ router.put("/:id", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) => {
-  // delete on tag by its `id` value
+  // deletes tag by its `id` value
   try {
     const deletedTag = await Tag.destroy({
       where: {id: req.params.id}
